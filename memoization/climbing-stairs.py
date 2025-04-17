5862c347686688256f75fd3a636debd6b7daddee
Eric-Guo12138 <Eric@vozeix.xyz>
2025-04-17 18:20:38 +0800

    LC-climbing-stairs [Runtime: N/A; Memory: N/A]

--- memoization/climbing-stairs.py
+++ memoization/climbing-stairs.py
@@ -1,10 +1,6 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        if n == 0:
-            return 0
-        elif n == 1:
-            return 1
-        elif n == 2:
-            return 2
+        if n <= 2:
+            return n
         else:
             return self.climbStairs(n-1) + self.climbStairs(n-2)
