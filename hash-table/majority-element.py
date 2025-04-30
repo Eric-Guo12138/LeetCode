d13bdfa1a8da1e31dec850ae6f8a66f536dbc672
Eric-Guo12138 <Eric@vozeix.xyz>
2025-04-30 16:21:27 +0800

    LC-majority-element [Runtime: 1 ms; Memory: 19.3 MB]

--- hash-table/majority-element.py
+++ hash-table/majority-element.py
@@ -1,5 +1,6 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        # 将数组排序，发现下标为len//2的元素就是众数
+        # 将数组排序，发现下标为len//2的元素就是多数
+        # 原因：出现次数>=n/2，那么无论多数位置在数组左、中、右，肯定有一个多数会在中间
         nums.sort()
         return nums[len(nums)//2]
