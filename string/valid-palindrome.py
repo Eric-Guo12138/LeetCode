964ab9dad5968079dd2e2a82a3d1dc8ef6da9ba4
Eric-Guo12138 <Eric@vozeix.xyz>
2025-05-28 18:30:25 +0800

    LC-valid-palindrome [Runtime: 7 ms; Memory: 18.5 MB]

--- string/valid-palindrome.py
+++ string/valid-palindrome.py
@@ -1,18 +1,15 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        # 将s的所有字母转换成小写，从后往前遍历s，如果有非字母数字字符，删除
-        # 前后分别设置两个指针，如果s[i]!=s[j]，返回False,否则i+1,j-1,直到i>=j
+        # 将s的所有字母转换成小写，遍历s，将所有字母数字字符保存到t中
+        # 前后分别设置两个指针，如果t[i]!=t[j]，返回False,否则i+1,j-1,直到i>=j
         s = s.lower()
-        m = len(s) - 1
-        while m >= 0:
-            if not s[m].isalnum():
-                s = s.replace(s[m],'')
-                m -= 1
-            else:
-                m -= 1
-        i,j = 0,len(s)-1
+        t = []
+        for i in s:
+            if i.isalnum():
+                t.append(i)
+        i,j = 0,len(t)-1
         while i < j:
-            if s[i] == s[j]:
+            if t[i] == t[j]:
                 i += 1
                 j -= 1
             else:
