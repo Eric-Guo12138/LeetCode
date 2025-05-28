41038354013fdca003d2107d17413a3f729d6bd3
Eric-Guo12138 <Eric@vozeix.xyz>
2025-05-28 18:16:39 +0800

    LC-valid-palindrome [Runtime: N/A; Memory: N/A]

---
+++ two-pointers/valid-palindrome.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        # 将s的所有字母转换成小写，从后往前遍历s，如果有非字母数字字符，删除
+        # 前后分别设置两个指针，如果s[i]!=s[j]，返回False,否则i+1,j-1,直到i>=j
+        s = s.lower()
+        m = len(s) - 1
+        while m >= 0:
+            if not s[m].isalnum():
+                s = s.replace(s[m],'')
+            m -= 1
+        i,j = 0,len(s)-1
+        while i < j:
+            if s[i] == s[j]:
+                i += 1
+                j -= 1
+            else:
+                return False
+        return True
