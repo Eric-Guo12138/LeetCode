f7fd097f47f0d4836080e376bcea77ebe7800449
Eric-Guo12138 <Eric@vozeix.xyz>
2025-06-05 16:09:39 +0800

    LC-n-th-tribonacci-number [Runtime: 0 ms; Memory: 18 MB]

--- math/n-th-tribonacci-number.py
+++ math/n-th-tribonacci-number.py
@@ -1,5 +1,9 @@
 class Solution:
     def tribonacci(self, n: int) -> int:
+        # 尝试了递归方法，超时。
+        # 设置了一个数组用来存放每个泰波那契数的值，n>=3时，计算出该值加到数组中
+        # 返回最后一个元素的值即可
+        ans = [0,1,1]
         if n == 0:
             return 0
         elif n == 1:
@@ -7,4 +11,6 @@ class Solution:
         elif n == 2:
             return 1
         else:
-            return self.tribonacci(n-1)+ self.tribonacci(n-2) + self.tribonacci(n-3)
+            for i in range(3,n+1):
+                ans.append(ans[i-1] + ans[i-2] + ans[i-3])
+            return ans[n]
