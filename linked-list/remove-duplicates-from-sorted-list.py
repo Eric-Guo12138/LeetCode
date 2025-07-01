609ab0a02b77ba6e558b39713b15db2cb0221637
Eric-Guo12138 <Eric@vozeix.xyz>
2025-07-01 13:58:16 +0800

    LC-remove-duplicates-from-sorted-list [Runtime: 3 ms; Memory: 18 MB]

--- linked-list/remove-duplicates-from-sorted-list.py
+++ linked-list/remove-duplicates-from-sorted-list.py
@@ -7,6 +7,8 @@ class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
         # 循环遍历链表，因为是已排序，所以如果下个结点值等于当前结点，就跳过
         # 否则就移动到下个结点
+        if head is None:
+            return None
         cur = head
         while cur.next:
             if cur.next.val == cur.val:
